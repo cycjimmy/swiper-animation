1c5d5421fa5562add40072947ed2b643307c017d
cycjimmy <cycjimmy@gmail.com>
2020-05-15 10:16:51 +0800

    chore(deps): replace moved rollup plugins; upgrade deps; adjust CI;

--- rollup/rollup.common.js
+++ rollup/rollup.common.js
@@ -1,9 +1,9 @@
 import { eslint } from 'rollup-plugin-eslint';
-import json from 'rollup-plugin-json';
-import resolve from 'rollup-plugin-node-resolve';
-import commonjs from 'rollup-plugin-commonjs';
-import babel from 'rollup-plugin-babel';
-import { terser } from 'rollup-plugin-terser/index';
+import json from '@rollup/plugin-json';
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import babel from '@rollup/plugin-babel';
+import { terser } from 'rollup-plugin-terser';
 
 import myBanner from '@cycjimmy/config-lib/chore/myBanner';
 // config
